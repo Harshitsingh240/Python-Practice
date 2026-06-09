1c6ccc2bc6f036fe01d845d632a39bdfdaacc14e
Harshitsingh240 <Harshitsinghm14@gmail.com>
2026-06-09 22:09:36 +0530

    Add files via upload

---
+++ 56.data.py
@@ -0,0 +1,16 @@
+#  Program Using Arithmetic Operators
+
+a = 10
+b = 5
+
+print("Add:", a + b)
+print("Sub:", a - b)
+print("Multip:", a * b)
+print("Divi:", a / b)
+print("Modulus:", a % b)
+print("Exponent:", a ** b)
+print("Floor Division:", a // b)
+
+
+
+
