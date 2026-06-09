6f0e3cc2143123ea5e30eb2ebd87cff4e0a50b51
Harshitsingh240 <Harshitsinghm14@gmail.com>
2026-06-09 22:03:41 +0530

    Add files via upload

---
+++ 28.comm.py
@@ -0,0 +1,11 @@
+ # Write a program explaining code using comments.
+
+
+
+# Ask user for their name
+name = input("Enter your name: ")
+
+# Print greeting message
+print("Hello, " + name )
+
+# End of program
