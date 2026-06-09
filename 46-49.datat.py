c9ef5fed97a1b9c4a659bc6214f11359a2ab0262
Harshitsingh240 <Harshitsinghm14@gmail.com>
2026-06-09 22:07:06 +0530

    Add files via upload

---
+++ 46-49.datat.py
@@ -0,0 +1,14 @@
+#Create integer variables.
+int_var = 10
+
+
+#Create float variables.
+float_var = 15.5
+
+
+#Create string variables.
+string_var = "Hello, Python!"
+
+
+#Create boolean variables.
+bool_var = True
