89137b8784fbf228687b55d25223dc4fbbc54676
Harshitsingh240 <Harshitsinghm14@gmail.com>
2026-06-09 22:16:16 +0530

    Add files via upload

---
+++ 73.condti.py
@@ -0,0 +1,14 @@
+# Write a program to display grade based on marks.
+
+marks = int(input("enter marks: "))
+
+if marks >= 90 :
+    print("grade = A")
+elif marks >= 75:
+    print("grade = B")
+elif marks >= 50:
+    print("grade = C")
+elif marks >= 30:
+    print("grade = D")
+else:
+    print("grade = F")
