5837ef0ca23c6ef3df0bf67d343d124bd45f45ff
Harshitsingh240 <Harshitsinghm14@gmail.com>
2026-06-09 22:10:24 +0530

    Add files via upload

---
+++ 59.data.py
@@ -0,0 +1,17 @@
+# Write a program using assignment operators.
+
+
+a = 10
+a += 5
+
+print("a += 5 :", a)
+
+a -= 3
+print("a -= 3 :", a)
+
+a *= 2
+print("a *= 2 :", a)
+
+a /= 4
+print("a /= 4 :", a)
+
