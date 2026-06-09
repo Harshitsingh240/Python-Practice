56b2c6058268448746b4e6967b38a351411eec85
Harshitsingh240 <Harshitsinghm14@gmail.com>
2026-06-09 22:05:42 +0530

    Add files via upload

---
+++ 40.multip.py
@@ -0,0 +1,10 @@
+# Write a program using chained assignment.
+# Chained assignment means assigning the same value to multiple variables in a single line.
+
+
+a = b = c = 100
+
+# Print values
+print("a =", a)
+print("b =", b)
+print("c =", c)
