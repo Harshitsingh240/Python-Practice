60e4e391aa0c2ca0e5071ee19db2ad5f97ab25b5
Harshitsingh240 <Harshitsinghm14@gmail.com>
2026-06-09 22:03:57 +0530

    Add files via upload

---
+++ 29.comm.py
@@ -0,0 +1,14 @@
+  # Write a commented program to calculate sum.
+
+
+# take the first number
+num1 = int(input("Enter first number: "))
+
+# take the second number
+num2 = int(input("Enter second number: "))
+
+#calculate sum
+result = num1 + num2
+
+#dispaly the result .
+print(result)
