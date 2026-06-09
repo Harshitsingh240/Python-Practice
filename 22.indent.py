6aa0b9e3dfd0808f1f5e2cb735963e5e4a2a2c87
Harshitsingh240 <Harshitsinghm14@gmail.com>
2026-06-09 22:02:12 +0530

    Add files via upload

---
+++ 22.indent.py
@@ -0,0 +1,17 @@
+# Write a nested if program with correct indentation.
+
+
+number = 11
+
+if number > 0:
+
+    print("positive number")
+
+    if number > 10:
+
+        print("Number is greater than 10")
+
+    else:
+            print("Number is less than 10")
+
+print("Done")
