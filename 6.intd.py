e6e3ef91ed3a210c12f04a515353ceffcca136eb
Harshitsingh240 <Harshitsinghm14@gmail.com>
2026-06-09 21:53:20 +0530

    Add files via upload

---
+++ 6.intd.py
@@ -0,0 +1,16 @@
+#Write a program to swap two numbers temporarily .
+
+a = 10
+b = 20
+print("before swap")
+print(a)
+print(b)
+
+temp = a
+a = b
+b = temp
+print("after swap")
+print(a)
+print(b)
+
+
