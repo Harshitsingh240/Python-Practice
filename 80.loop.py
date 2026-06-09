2aa615e3772d7a1a9b7ee757463be34de573cb08
Harshitsingh240 <Harshitsinghm14@gmail.com>
2026-06-09 22:19:44 +0530

    Add files via upload

---
+++ 80.loop.py
@@ -0,0 +1,9 @@
+# Write a program to find factorial of a number.
+
+num = int(input("Enter a number: "))
+factorial = 1
+
+for i in range(1, num + 1):
+    factorial *= i
+
+print("Factorial =", factorial)
