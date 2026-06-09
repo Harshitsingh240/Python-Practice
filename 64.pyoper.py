11351c0a999a11d34fb32e42af8aac09e771a955
Harshitsingh240 <Harshitsinghm14@gmail.com>
2026-06-09 22:13:10 +0530

    Add files via upload

---
+++ 64.pyoper.py
@@ -0,0 +1,10 @@
+# Find largest among two numbers.
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if num1 > num2:
+    print("Largest number is:", num1)
+else:
+    print("Largest number is:", num2)
+
