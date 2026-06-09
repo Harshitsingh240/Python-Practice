0eabc0824b2f5acd8076409ae8f4fce82d6e5422
Harshitsingh240 <Harshitsinghm14@gmail.com>
2026-06-09 22:00:37 +0530

    Add files via upload

---
+++ 20.arith.py
@@ -0,0 +1,8 @@
+# 10. Program to calculate average of 3 numbers
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+c = float(input("Enter third number: "))
+
+average = (a + b + c) / 3
+print(average)
