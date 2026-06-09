9a1606e8eaac47389e7e5d5c164249da295e48ac
Harshitsingh240 <Harshitsinghm14@gmail.com>
2026-06-09 22:10:42 +0530

    Add files via upload

---
+++ 60.data.py
@@ -0,0 +1,20 @@
+# Write a program using membership operators.
+
+# operation is  ((in and not in )):
+
+fruits = ["apple","banana","mango"]
+
+print("apple in friuts")
+print("graphs in fruits")
+
+print("banana not in friuts")
+print("graphs not in fruits")
+
+
+
+
+
+
+
+
+
