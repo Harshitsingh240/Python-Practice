40d58e393f84ec75795b5dcdcbc1f1ca9bcc5ac6
Harshitsingh240 <Harshitsinghm14@gmail.com>
2026-06-09 22:07:19 +0530

    Add files via upload

---
+++ 50.datat.py
@@ -0,0 +1,7 @@
+# Print data type using type() function.
+
+
+print("Data type  int_var:", type(int_var))
+print("Data type  float_var:", type(float_var))
+print("Data type  string_var:", type(string_var))
+print("Data type  bool_var:", type(bool_var))
